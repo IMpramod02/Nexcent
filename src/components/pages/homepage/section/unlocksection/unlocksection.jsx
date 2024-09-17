088f13ee349e641f0d2ed11c85d59f5731f0c803
IMpramod02 <pramod01248@gmail.com>
2024-09-17 12:03:04 +0530

    w

--- src/components/pages/homepage/section/unlocksection/unlocksection.jsx
+++ src/components/pages/homepage/section/unlocksection/unlocksection.jsx
@@ -1,26 +1,4 @@
 /* eslint-disable react/prop-types */
-/*import './UnlockSection.scss';
-import { HOME_PAGE_CONSTANTS } from './unlocksection';
-
-const UnlockSection = () => {
-  const { heading, paragraph, buttonLabel, imageAlt, imageUrl } =
-    HOME_PAGE_CONSTANTS;
-
-  return (
-    <section className="unlock-section-container">
-      <div className="banner-section">
-        <img src={imageUrl} alt={imageAlt} className="mobil-image" />
-      </div>
-      <div className="content-section">
-        <h2>{heading}</h2>
-        <p>{paragraph}</p>
-        <button className="learn-more-button">{buttonLabel}</button>
-      </div>
-    </section>
-  );
-};
-
-export default UnlockSection;*/
 
 import './UnlockSection.scss';
 const UnlockSection = ({
