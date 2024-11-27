918eac343bbf1c80828d78e0a18d0301a920d98a
IMpramod02 <pramod01248@gmail.com>
2024-11-27 18:11:06 +0530

    suspense and lazyloading

--- src/main.jsx
+++ src/main.jsx
@@ -16,25 +16,26 @@
 // import LoginForm from './forms/loginform/loginform';
 // import DashboardPage from './components/pages/homepage/section/dashboardsection/dashboard';
 // import { LanguageProvider } from './context/LanguageContext';
+// import AuthCheck from './components/global/AuthCheck';
 
 // function App() {
 //   return (
 //     <Router>
 //       <LanguageProvider>
 //         <Navbar />
-//         <Routes>
-//           <Route path="/:lang" element={<MainLayout />}>
-//             <Route path="home" element={<HomePage />}>
-//               <Route path="signup" element={<SignUpForm />} />
-//               <Route path="signup/step2" element={<SignUpFormStepTwo />} />
-//               <Route path="login" element={<LoginForm />} />
+//         <AuthCheck>
+//           <Routes>
+//             <Route path="/:lang" element={<MainLayout />}>
+//               <Route path="home" element={<HomePage />} />
+//               <Route path="home/signup" element={<SignUpForm />} />
+//               <Route path="home/signup/step2" element={<SignUpFormStepTwo />} />
+//               <Route path="home/login" element={<LoginForm />} />
+//               <Route path="faq" element={<FAQPage />} />
+//               <Route path="dashboard" element={<DashboardPage />} />
 //             </Route>
-//             <Route path="faq" element={<FAQPage />} />
-//             <Route path="dashboard" element={<DashboardPage />} />
-//           </Route>
-
-//           <Route path="/" element={<Navigate to="/en/home" />} />
-//         </Routes>
+//             <Route path="/" element={<Navigate to="/en/home" />} />
+//           </Routes>
+//         </AuthCheck>
 //       </LanguageProvider>
 //     </Router>
 //   );
@@ -54,7 +55,7 @@
 
 // export default App;
 
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom/client';
 import {
   BrowserRouter as Router,
@@ -65,40 +66,45 @@ import {
 } from 'react-router-dom';
 import './styles/global.scss';
 import Navbar from './components/global/navbar/Navbar';
-import HomePage from './pages/homepage/homepage';
-import FAQPage from './components/pages/homepage/section/faqsection/faqpage';
-import SignUpForm from './forms/signupform/signupform';
-import SignUpFormStepTwo from './forms/signupform/SignUpFormStepTwo';
-import LoginForm from './forms/loginform/loginform';
-import DashboardPage from './components/pages/homepage/section/dashboardsection/dashboard';
 import { LanguageProvider } from './context/LanguageContext';
 import AuthCheck from './components/global/AuthCheck';
+
+const HomePage = lazy(() => import('./pages/homepage/homepage'));
+const FAQPage = lazy(() =>
+  import('./components/pages/homepage/section/faqsection/faqpage')
+);
+const SignUpForm = lazy(() => import('./forms/signupform/signupform'));
+const SignUpFormStepTwo = lazy(() =>
+  import('./forms/signupform/SignUpFormStepTwo')
+);
+const LoginForm = lazy(() => import('./forms/loginform/loginform'));
+const DashboardPage = lazy(() =>
+  import('./components/pages/homepage/section/dashboardsection/dashboard')
+);
 
 function App() {
   return (
     <Router>
       <LanguageProvider>
         <Navbar />
-        <Routes>
-          <Route path="/:lang" element={<MainLayout />}>
-            <Route path="home" element={<HomePage />}>
-              <Route path="signup" element={<SignUpForm />} />
-              <Route path="signup/step2" element={<SignUpFormStepTwo />} />
-              <Route path="login" element={<LoginForm />} />
-            </Route>
-            <Route path="faq" element={<FAQPage />} />
-
-            <Route
-              path="dashboard"
-              element={
-                <AuthCheck>
-                  <DashboardPage />
-                </AuthCheck>
-              }
-            />
-          </Route>
-          <Route path="/" element={<Navigate to="/en/home" />} />
-        </Routes>
+        <AuthCheck>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/:lang" element={<MainLayout />}>
+                <Route path="home" element={<HomePage />} />
+                <Route path="home/signup" element={<SignUpForm />} />
+                <Route
+                  path="home/signup/step2"
+                  element={<SignUpFormStepTwo />}
+                />
+                <Route path="home/login" element={<LoginForm />} />
+                <Route path="faq" element={<FAQPage />} />
+                <Route path="dashboard" element={<DashboardPage />} />
+              </Route>
+              <Route path="/" element={<Navigate to="/en/home" />} />
+            </Routes>
+          </Suspense>
+        </AuthCheck>
       </LanguageProvider>
     </Router>
   );
